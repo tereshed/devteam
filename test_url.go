821de09ceac14be961dbba31026772655c540d13
Kostya <santy.teresh@laventa.ai>
2026-05-16 16:09:45 +0200

    remove files

--- test_url.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-	"fmt"
-	"net/url"
-)
-func main() {
-	u, _ := url.Parse("https://[::1]:8080/foo")
-	fmt.Printf("Host: %q, Hostname: %q, Port: %q\n", u.Host, u.Hostname(), u.Port())
-}
--- test_userinfo.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-	"fmt"
-	"net/url"
-)
-func main() {
-	u := url.UserPassword("x-access-token", "my pass+word")
-	fmt.Println(u.String())
-}
